0c7a1a22983877425daf284c825e9e7c7b02f68f
PoManToeMan <rebeccamafileo@gmail.com>
2026-07-01 15:21:57 +1200

    Able to intialise and display a predefined transaction in plain text. Testing purposes.

--- app/src/main/java/com/example/mtfinance/MainActivity.java
+++ app/src/main/java/com/example/mtfinance/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.mtfinance;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+import com.example.mtfinance.src.Transaction;
+import java.time.LocalDateTime;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Transaction transaction = new Transaction(123, "Hello world", LocalDateTime.of(2007, 12, 1, 5, 30), 1);
+
+        TextView textview = findViewById(R.id.textView);
+        textview.setText(transaction.toString());
+
+
     }
 }
--- app/src/main/java/com/example/mtfinance/src/Transaction.java
+++ app/src/main/java/com/example/mtfinance/src/Transaction.java
@@ -1,20 +1,32 @@
 package com.example.mtfinance.src;
 
+import androidx.annotation.NonNull;
+
+import java.time.LocalDateTime;
+
 public class Transaction {
     // instance fields
     private final Integer Id;
-    private String description;
-    private final long date;
+    private final String description;
+    private final float amount;
+    private final LocalDateTime date;
 
     // constructor
-    public Transaction(Integer Id, String description, long date) {
+    public Transaction(Integer Id, String description, LocalDateTime date, float amount) {
         this.Id = Id;
         this.description = description;
         this.date = date;
+        this.amount = amount;
     }
 
     public Integer getId() {
         return Id;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format("Id: %s\n desc: %s \n date: %s", Id, description, date);
     }
 
 
