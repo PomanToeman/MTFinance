a39dcb7d1e10fb53d59ddb7531c57e61730e8cd5
PoManToeMan <rebeccamafileo@gmail.com>
2026-07-05 15:52:01 +1200

    Added insert methods into tracking Repository, as well as findCategoriesByTransactionID method, which will be useful for later methods. Will test in next commit.

--- app/src/main/java/com/example/mtfinance/src/TrackingRepository.java
+++ app/src/main/java/com/example/mtfinance/src/TrackingRepository.java
@@ -1,6 +1,11 @@
 package com.example.mtfinance.src;
 
 
+import androidx.annotation.NonNull;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * This is the main Tracking repository class.
  * To store and extract categories and transactions together from the room database.
@@ -11,9 +16,65 @@ public class TrackingRepository {
     private final CategoryRepository categoryRepository;
     private final TransactionRepository transactionRepository;
 
+    // constructor
     public TrackingRepository(CategoryRepository categoryRepository, TransactionRepository transactionRepository) {
         this.categoryRepository = categoryRepository;
         this.transactionRepository = transactionRepository;
+    }
+
+    /**
+     * Insert a category into the database.
+     * Warining: this method assumes any chanced transactions are already in the database (as it only brings the IDs).
+     *  this does not insert/update the cached transactions into the database. Must be done separately.
+     * @param category - the category to be inserted
+     */
+    public void insertCategory(Category category) {
+        categoryRepository.insert(category);
+    }
+
+    /**
+     * If you just want to insert a transaction with a specific category, use this method.
+     * The Category must already be in the database.
+     * It's best to insert the category first before inserting the transaction.
+     *
+     * @param transaction - The transaction to be inserted.
+     * @param CategoryId - The ID of a category already within a database.
+     */
+    public void insertTransaction(@NonNull Transaction transaction, @NonNull Long CategoryId) {
+        Category category = categoryRepository.getCategoryById(CategoryId);
+        if (category == null) {
+            return;
+        }
+        category.addTransaction(transaction);
+        categoryRepository.updateCategory(category);
+
+
+        transactionRepository.insert(transaction);
+    }
+
+    /**
+     * If you just want to insert a transaction with no specific category, use this method.
+     * @param transaction - The transaction to be inserted.
+     */
+    public void insertTransactionDefault(@NonNull Transaction transaction) {
+        Category generalCategory = categoryRepository.getGeneralCategory();
+        generalCategory.addTransaction(transaction);
+        categoryRepository.updateCategory(generalCategory);
+        transactionRepository.insert(transaction);
+
+    }
+
+    /**
+     * finds all categories that has a given transaction with the same ID.
+     *
+     * @param id - The given ID of the transaction
+     * @return - returns a set of categories that has the given transaction ID.
+     */
+     public Set<Category> findCategoriesByTransactionId(Long id) {
+
+        return categoryRepository.getAllCategories().stream()
+                .filter(category -> category.getTransactionIds().contains(id))
+                .collect(Collectors.toSet());
     }
 
 }
