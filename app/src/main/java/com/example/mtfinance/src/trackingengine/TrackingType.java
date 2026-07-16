aa9d3505dbe339dce75de390b03e63ceb519c367
PoManToeMan <rebeccamafileo@gmail.com>
2026-07-16 23:37:33 +1200

    Expand TrackingType enums to have a toString static method (with ability to get Type off other names but same meaning).

--- app/src/main/java/com/example/mtfinance/src/trackingengine/TrackingType.java
+++ app/src/main/java/com/example/mtfinance/src/trackingengine/TrackingType.java
@@ -1,5 +1,32 @@
 package com.example.mtfinance.src.trackingengine;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public enum TrackingType {
-    EXPENSE, INCOME, ACCOUNT_TRANSFERS;
+    EXPENSE("expense", "debit", "withdrawal"),
+    INCOME("income", "credit", "deposit"),
+    ACCOUNT_TRANSFERS("account transfers", "transfer", "money transfer", "savings"),
+    OTHER("other", "unknown"); // for errors
+
+    private final Set<String> otherNames = new HashSet<>();
+
+    TrackingType(String... otherNames) {
+        this.otherNames.addAll(Set.of(otherNames));
+    }
+
+    public static TrackingType fromString(String name) {
+        if (name == null) {
+            return OTHER;
+        }
+        String cleanName = name.trim().toLowerCase();
+        for (TrackingType type : values()) {
+            if (type.name().equalsIgnoreCase(cleanName) || type.otherNames.contains(cleanName)) {
+                return type;
+            }
+        }
+        return OTHER;
+    }
+
+
 }
